b49a88c85b70e9a382b22e44468b759912d4ab1e
ZiedYousfi <yousfi_zied@yahoo.com>
2025-12-29 10:24:05 +0100

    refactor: replace hardcoded French words with embedded dictionary file

--- spellchecker.go
+++ spellchecker.go
@@ -1,34 +1,31 @@
 package main
 
 import (
+	"embed"
+	"strings"
+
 	spellchecker "github.com/f1monkey/spellchecker/v3"
 )
 
-// French words list for dictionary
-var frenchWords = []string{
-	"bonjour", "merci", "salut", "bonsoir", "oui", "non", "peut", "être", "avoir", "faire",
-	"aller", "venir", "dire", "pouvoir", "voir", "savoir", "vouloir", "temps", "année", "jour",
-	"homme", "femme", "enfant", "chose", "vie", "monde", "pays", "maison", "ville", "rue",
-	"monsieur", "madame", "grand", "petit", "bon", "mauvais", "beau", "nouveau", "vieux", "jeune",
-	"français", "français", "anglais", "espagnol", "allemand", "italien", "européen", "américain",
-	"travail", "école", "livre", "ordinateur", "téléphone", "voiture", "table", "chaise", "porte", "fenêtre",
-	"manger", "boire", "dormir", "parler", "écrire", "lire", "écouter", "regarder", "aimer", "détester",
-	"heureux", "triste", "content", "fâché", "surpris", "fatigué", "malade", "sain", "fort", "faible",
-	"aujourd", "demain", "hier", "maintenant", "toujours", "jamais", "souvent", "parfois", "rarement",
-	"ici", "là", "partout", "nulle", "part", "quelque", "part", "ailleurs", "dedans", "dehors",
-	"comment", "pourquoi", "quand", "combien", "lequel", "laquelle", "lesquels", "lesquelles",
-	"avec", "sans", "pour", "contre", "chez", "vers", "dans", "sur", "sous", "devant",
-	"derrière", "entre", "parmi", "durant", "pendant", "avant", "après", "depuis", "jusqu",
-	"café", "thé", "eau", "vin", "pain", "fromage", "viande", "poisson", "légume", "fruit",
-	"pomme", "orange", "banane", "tomate", "carotte", "salade", "poulet", "boeuf", "porc",
-	"rouge", "bleu", "vert", "jaune", "noir", "blanc", "gris", "rose", "violet", "orange",
-	"un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix",
-	"vingt", "trente", "quarante", "cinquante", "soixante", "cent", "mille", "million",
-	"père", "mère", "fils", "fille", "frère", "soeur", "oncle", "tante", "cousin", "cousine",
-	"ami", "amie", "copain", "copine", "voisin", "voisine", "collègue", "patron", "client",
-	"janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre",
-	"novembre", "décembre", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche",
-	"matin", "midi", "après", "soir", "nuit", "heure", "minute", "seconde", "semaine", "mois",
+//go:embed francais.txt
+var frenchDictFS embed.FS
+
+// frenchWords holds the loaded French words from the embedded file
+var frenchWords []string
+
+func init() {
+	data, err := frenchDictFS.ReadFile("francais.txt")
+	if err != nil {
+		panic("failed to read embedded francais.txt: " + err.Error())
+	}
+
+	lines := strings.Split(string(data), "\n")
+	for _, line := range lines {
+		word := strings.TrimSpace(line)
+		if word != "" {
+			frenchWords = append(frenchWords, word)
+		}
+	}
 }
 
 // NewFrenchSpellchecker creates and initializes a spellchecker with French dictionary
